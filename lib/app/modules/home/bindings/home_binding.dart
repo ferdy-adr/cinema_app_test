4847f57080391291332001c2ac9e5b39f33a9778
Ferdy Adriantama <ferdypbg@gmail.com>
2024-07-17 21:07:17 +0700

    feat: Complete page Home
    Create UI; Consume data from API;

--- lib/app/modules/home/bindings/home_binding.dart
+++ lib/app/modules/home/bindings/home_binding.dart
@@ -1,3 +1,6 @@
+import 'package:cinema_app_test/app/data/repository/movie_repository.dart';
+import 'package:cinema_app_test/app/data/repository_impl/movie_repository_impl.dart';
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
@@ -7,6 +10,12 @@ class HomeBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<HomeController>(
       () => HomeController(),
+    );
+
+    Get.lazyPut<MovieRepository>(
+      () => MovieRepositoryImpl(
+        client: Get.find<Dio>(),
+      ),
     );
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,126 @@
+import 'package:cinema_app_test/app/data/models/movie.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
+  const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('HomeView'),
+        backgroundColor: const Color(0xFF2E236C),
+        title: const Text(
+          'HomeView',
+          style: TextStyle(
+            color: Color(0xFFFFFFFF),
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: FutureBuilder(
+        future: controller.movieRepository.getMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Text(
+              'Terjadi kesalahan, silahkan coba beberapa saat lagi. (${snapshot.error})',
+            );
+          } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            final movies = snapshot.data!;
+
+            return CustomScrollView(
+              slivers: [
+                SliverPadding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) => _MovieItem(movies[index]),
+                      childCount: movies.length,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return const Text('Tidak ditemukan data');
+          }
+        },
+      ),
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 12,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xFF987D9A),
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image(
+              image: NetworkImage(movie.image),
+              height: 80,
+            ),
+          ),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Text(
+              movie.title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          GestureDetector(
+            onTap: () {
+              Get.dialog(
+                Center(
+                  child: Container(
+                    height: 250,
+                    width: 250,
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: QrImageView(data: movie.id),
+                  ),
+                ),
+              );
+            },
+            child: const Icon(
+              Icons.qr_code,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
